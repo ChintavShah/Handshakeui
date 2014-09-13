c414aac68c1337c0a84f473b9b365f822f677130
ChintavShah <chintav7@yahoo.com>
2014-09-13 16:02:33 -0400

    storyboard

---
+++ HandshakeUI/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HandshakeUI
+//
+//  Created by Andy  on 9/13/14.
+//  Copyright (c) 2014 ChintavShah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HandshakeUI/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HandshakeUI
+//
+//  Created by Andy  on 9/13/14.
+//  Copyright (c) 2014 ChintavShah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
